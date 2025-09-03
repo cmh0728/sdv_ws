b5168ba5a12c29c3234ea419b4ac78291bd8080b
cmh0728 <gambaro344@gmail.com>
2025-09-03 12:29:09 +0200

    sucess to connect remote server

--- src/carla/carla_test.py
+++ src/carla/carla_test.py
@@ -5,6 +5,7 @@ def main():
     # Connect to the client and retrieve the world object
     client = carla.Client('192.168.86.74', 2000) # 기본2000번 포트, 다 사용가능.
     world = client.get_world()
+    client.load_world('Town05')
     print('sucess to connect server with client') # 디버깅 확인 완료. 연결됨 
 
 if __name__ == "__main__":
