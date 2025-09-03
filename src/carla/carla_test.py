5204402d1f2101fe13cabef0d74d3766821452d5
cmh0728 <gambaro344@gmail.com>
2025-09-03 13:25:42 +0200

    edit  moving code

--- src/carla/carla_test.py
+++ src/carla/carla_test.py
@@ -3,10 +3,24 @@ import random
 
 def main():
     # Connect to the client and retrieve the world object
-    client = carla.Client('192.168.86.74', 2000) # 기본2000번 포트, 다 사용가능.
+    client = carla.Client('localhost', 2000) # 기본2000번 포트, 다 사용가능.
     world = client.get_world()
-    client.load_world('Town05')
+    client.load_world('Town03') # 맵 바꾸기
     print('sucess to connect server with client') # 디버깅 확인 완료. 연결됨 
+
+    # Retrieve the spectator object
+    spectator = world.get_spectator()
+
+    # Get the location and rotation of the spectator through its transform
+    transform = spectator.get_transform()
+
+    location = transform.location
+    rotation = transform.rotation
+
+    # Set the spectator with an empty transform
+    spectator.set_transform(carla.Transform())
+    # This will set the spectator at the origin of the map, with 0 degrees
+    # pitch, yaw and roll - a good way to orient yourself in the map
 
 if __name__ == "__main__":
     main()
