22008bb02c1e88de7c45c87ba9aa9b7a81420469
cmh0728 <gambaro344@gmail.com>
2025-09-03 11:53:31 +0200

    edit localhost

--- src/carla/carla_test.py
+++ src/carla/carla_test.py
@@ -3,9 +3,9 @@ import random
 
 def main():
     # Connect to the client and retrieve the world object
-    client = carla.Client('localhost', 2000)
+    client = carla.Client('192.168.86.74', 2000) # 기본2000번 포트, 다 사용가능.
     world = client.get_world()
-    print('sucess to connect server with client')
+    print('sucess to connect server with client') # 디버깅 확인 완료. 연결됨 
 
 if __name__ == "__main__":
     main()
