576ba75be298ade551c3bca0b6d8b7bd7d4e6764
cmh0728 <gambaro344@gmail.com>
2025-09-03 13:29:06 +0200

    edit localhost

--- src/carla/carla_test.py
+++ src/carla/carla_test.py
@@ -3,8 +3,8 @@ import random
 
 def main():
     # Connect to the client and retrieve the world object
-    client = carla.Client('192.168.86.74', 2000) # 기본2000번 포트, 다 사용가능.동민이형 
-    # client = carla.Client('localhost', 2000) # 기본2000번 포트, 다 사용가능.
+    # client = carla.Client('192.168.86.74', 2000) # 기본2000번 포트, 다 사용가능.동민이형 
+    client = carla.Client('localhost', 2000) # 기본2000번 포트, 다 사용가능.
 
     world = client.get_world()
     client.load_world('Town03') # 맵 바꾸기
